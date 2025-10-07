8a769170453862f6ec7431123add3d79187de5fc
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-10-07 14:20:10 +0200

    dada

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class btnSaludar
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.foto = new System.Windows.Forms.PictureBox();
+            this.progreso = new System.Windows.Forms.ProgressBar();
+            this.web1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.foto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(338, 405);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(195, 73);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Sorpresa";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // foto
+            // 
+            this.foto.Location = new System.Drawing.Point(631, 12);
+            this.foto.Name = "foto";
+            this.foto.Size = new System.Drawing.Size(183, 197);
+            this.foto.TabIndex = 3;
+            this.foto.TabStop = false;
+            this.foto.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // progreso
+            // 
+            this.progreso.Location = new System.Drawing.Point(366, 383);
+            this.progreso.Name = "progreso";
+            this.progreso.Size = new System.Drawing.Size(143, 29);
+            this.progreso.TabIndex = 5;
+            // 
+            // web1
+            // 
+            this.web1.Location = new System.Drawing.Point(40, 52);
+            this.web1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web1.Name = "web1";
+            this.web1.Size = new System.Drawing.Size(572, 325);
+            this.web1.TabIndex = 6;
+            this.web1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.web1_DocumentCompleted);
+            // 
+            // btnSaludar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 597);
+            this.Controls.Add(this.web1);
+            this.Controls.Add(this.progreso);
+            this.Controls.Add(this.foto);
+            this.Controls.Add(this.btn);
+            this.Name = "btnSaludar";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.foto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox foto;
+        private System.Windows.Forms.ProgressBar progreso;
+        private System.Windows.Forms.WebBrowser web1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class btnSaludar : Form
+    {
+        public btnSaludar()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Inicializa el progress bar
+            progreso.Minimum = 0;
+            progreso.Maximum = 100;
+            progreso.Value = 0;
+
+            // Simula el progreso
+            for (int i = 0; i <= 100; i++)
+            {
+                progreso.Value = i;
+                System.Threading.Thread.Sleep(10); // Simula trabajo
+                Application.DoEvents(); // Refresca el ProgressBar
+            }
+
+            // Cuando termina, muestra la imagen
+            try
+            {
+                web1.Navigate("https://www.wordreference.com/");
+                foto.Image = Image.FromFile(@"C:\Users\CAMPUSFP\Pictures\ChatGPT Image 17 sept 2025, 12_26_43.png");
+                foto.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo cargar la imagen: " + ex.Message);
+            }
+        }
+
+        private void Ikimamonazo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void web1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+    }
+}
